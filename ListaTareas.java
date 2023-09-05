4d93cf19441b33833821bff8f5efb18d382bab90
uwuounu <143724520+uwuounu@users.noreply.github.com>
2023-09-05 08:45:11 -0600

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package listatareas;
+
+/**
+ *
+ * @author labcca
+ */
+public class ListaTareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String tarea1 = "Apunte de la clase";
+        String tarea2 = "Linea del tiempo";
+        String tarea3 = "Investigación";
+        String tarea4 = "Presentacion en PowerPoint";
+        String tarea5 = "Formulario y ejercicios de calculo";       
+        
+        System.out.println("Tarea 1: "+tarea1);
+        System.out.println("Tarea 2: "+tarea2);
+        System.out.println("Tarea 3: "+tarea3);
+        System.out.println("Tarea 4: "+tarea4);
+        System.out.println("Tarea 5: "+tarea5);
+    }
+    
+}
